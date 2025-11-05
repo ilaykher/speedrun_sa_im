37028deb7d1176f31d5db2f1714407cfecf72ee2
IT <IT@DESKTOP-HF1DR36>
2025-11-06 02:39:17 +0800

    fix

---
+++ speedrun_sa_im/StudentPortalForm.vb
@@ -0,0 +1,12 @@
+﻿' ==========================================
+' FILENAME: /Forms/StudentPortalForm.vb
+' PURPOSE: Student portal interface for personal academic information access
+' AUTHOR: System
+' DATE: 2025-10-20
+' ==========================================
+Imports Guna.UI2.WinForms
+
+Public Class StudentPortalForm
+
+
+End Class
